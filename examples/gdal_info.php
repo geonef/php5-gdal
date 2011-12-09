360e1d1ea711b088560148bbdcd9af722f26e413
Okapi <okapi@geonef.fr>
2011-12-09 18:55:27 +0100

    added example: gdal_info.php

---
+++ examples/gdal_info.php
@@ -0,0 +1,30 @@
+<?php
+
+// In this example, GDAL is used to get basic information about a particular
+// raster dataset given in argument.
+
+
+function showInfo(GDALDataSet $dataset)
+{
+  echo "driver: ".$dataset->GetDriver()->GetDescription()."\n";
+  echo "width: ".$dataset->getRasterXSize()."\n";
+  echo "height: ".$dataset->getRasterYSize()."\n";
+  echo "number of bands: ".$dataset->GetRasterCount()."\n";
+  echo "projection: ".$dataset->GetProjectionRef()."\n";
+}
+
+
+$path =& $_SERVER['argv'][1];
+if (!isset($path)) {
+  throw new Exception("Missing argument: path to raster dataset");
+}
+
+GDALAllRegister();
+
+$dataset = GDALOpen($path, GA_ReadOnly);
+if (!$dataset) {
+  throw new Exception("Could not open raster dataset: ".$path);
+}
+
+header("Content-type: text/plain");
+showInfo($dataset);
